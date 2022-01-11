f3dcd6cbf796bc847175cc81248ac99a5b4676db
StefanoColamonaco <juststep.2000@gmail.com>
2022-01-11 12:15:23 +0100

    New version that provides support for files

--- textRetriever.py
+++ textRetriever.py
@@ -1,20 +1,32 @@
 from bs4 import BeautifulSoup
 import requests
-
-urls = []
+import json
 
 def fillURLs():
-    with open('urlsList.txt') as f:
-        lines = f.readlines()
-        for line in lines:
-            urls.append(line)
+    with open('urlsList.json') as f:
+        return json.load(f)
 
+def file(url):
+    #print("is a file", url)
+    response = requests.get(str(url))
+    text = response.text
+    #print(text)
+
+def site(url):
+    #print("is a site", url)
+    response = requests.get(str(url))
+    html = response.text
+
+    soup = BeautifulSoup(html, features="html.parser")
+    text = soup.get_text()
+    #print(text)
+
+options = {
+    "F": file,
+    "S": site
+}
 
 if __name__ == "__main__":
-    fillURLs()
-    for url in urls:
-        response = requests.get(str(url))
-        html = response.text
-
-        soup = BeautifulSoup(html, features="html.parser")
-        print(soup.get_text())
+    elements = fillURLs()
+    for element in elements:
+        options[element["type"]](element["url"])
