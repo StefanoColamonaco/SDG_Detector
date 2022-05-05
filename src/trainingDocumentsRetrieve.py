b806bf8045daa8708ad5fd858783f1c887178a04
StefanoColamonaco <juststep.2000@gmail.com>
2022-05-05 11:37:18 +0200

    Modified training documents retriever to find negative docs

--- src/trainingDocumentsRetrieve.py
+++ src/trainingDocumentsRetrieve.py
@@ -5,11 +5,12 @@ import time
 from textRetriever import site 
 
 NUMSENTENCESPERPARAGRAPH = 5
-DOCSPERSDG = 1
+DOCSPERSDG = 2
 SDGOBJECTIVES = [ "No poverty", "Zero hunger", "Good health and well-being", "Quality education", "Gender equality", "Clean water and sanitation", "Affordable and clean energy", "Decent work and economic growth", "Industry, innovation and infrastructure", "Reduced inequalities", "Sustainable cities and communities", "Responsible consumption and production", "Climate action", "Life below water", "Life on land", "Peace, justice and strong institutions", "Partnerships for the sustainable goals" ]
-TRAININGCOMPANIES = ["Nike","Adobe", "Ibm", "Nvidia","Wikipedia","Unibo", "Google", "Microsoft"]
+TRAININGCOMPANIES = ["Nike","Adobe", "Ibm", "Nvidia","Wikipedia","Unibo", "Google", "Microsoft","Tesla", "Adidas"]
+#TRAININGCOMPANIES = ["company"]
 
-def automaticTrainingDocumentRetrieve(newAnalysis):
+def automaticTrainingDocumentRetrieve(newAnalysis, positive=1):
     count = 0
     if(newAnalysis == 1):
         dataForRegister = []
@@ -18,7 +19,11 @@ def automaticTrainingDocumentRetrieve(newAnalysis):
             for company in TRAININGCOMPANIES:
                 print("\tFinding for Company named", company)
                 objective = SDGOBJECTIVES[objIndex]
-                query = objective+" "+"at "+company
+                query = objective+" "+"at "
+                if(positive == 1): 
+                    query = query + company
+                else:
+                    query = "Scandal " + query + "company"
                 time.sleep(1)                           # to avoid 429
                 try:
                     urls = search(query)
@@ -34,17 +39,29 @@ def automaticTrainingDocumentRetrieve(newAnalysis):
                     if (url.find(".pdf") == -1):
                         text = site(url)
                         data = splitTextIntoParagraphs(text)
-                        path = "../data/automatedTrainingURLs/documents/" 
+                        path = ""
+                        if(positive == 1):
+                            path = "../data/automatedTrainingURLs/documents/" 
+                        else:
+                            path = "../data/automatedTrainingURLs/negDocuments/"
                         filename = path+"document"+str(count+1)+".json"
                         with open(filename, 'w') as f:
                             json.dump(data, f, indent=2)
                         dataForRegister.append({"document": "document"+str(count+1), "SDG_Number":objIndex+1,"SDG":objective,"Company":company,"site":url})
                         count = count+1
-        filename = "../data/automatedTrainingURLs/documentsRegister.json" 
+        filename = ""
+        if(positive == 1):
+            filename = "../data/automatedTrainingURLs/documentsRegister.json" 
+        else:
+            filename = "../data/automatedTrainingURLs/negDocumentsRegister.json" 
         with open(filename, 'w') as f:
             json.dump(dataForRegister, f, indent=2)
     else:
-        filename = "../data/automatedTrainingURLs/documentsRegister.json" 
+        filename = ""
+        if(positive == 1):
+            filename = "../data/automatedTrainingURLs/documentsRegister.json" 
+        else:
+            filename = "../data/automatedTrainingURLs/negDocumentsRegister.json" 
         with open(filename, 'r') as f:
             registerData = json.load(f)
             count = len(registerData)
@@ -57,6 +74,7 @@ def splitTextIntoParagraphs(text):
     for i in range(0,len(sentences)):
         paragraph = paragraph + sentences[i] + " "
         if((i+1)&NUMSENTENCESPERPARAGRAPH == 0):
+            paragraph = " ".join(filter(lambda x:x[0]!="\\", paragraph.split()))
             data.append(paragraph)
             paragraph = ""
     return data
